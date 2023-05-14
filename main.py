8970728a83aa0d5d28cf760fb138120f98b935fa
Kyu Lee <kwooler@gmail.com>
2023-05-14 15:33:25 -0700

    CS7L1007

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 
-class Rectangle:
-    #########################################
+class Student:
+    numofStudent = 0
+
+    ########################################
     # Code your program here
-    #########################################
+    ########################################
+
+    def __str__(self):
+        strval = f'Name: {self._name:>10} \t Scores: '
+        for i in range(len(self._scores)):
+            strval += str(self._scores[i]) + '\t'
+        return strval
 
 
 def main():
-    r1 = Rectangle(10, 20)
-    r2 = Rectangle(100, 200)
-    print(r1)
-    print(r2)
-    r1._width = 99
-    r1._height = 99
-    print(r1)
+    sc1 = [100, 100, 100]
+    sc2 = [90, 90, 90]
 
-    if r1 > r2:
-        print('Rectangle r1 is greater than r2')
-    else:
-        print('Rectangle r1 is not greater than r2')
-    if r1 < r2:
-        print('Rectangle r1 is less than r2')
-    else:
-        print('Rectangle r1 is not less than r2')
+    s1 = Student('James', sc1)
+    s2 = Student('Mary', sc2)
+    print(s1)
+    print(s2)
+    print(f'Total number of students: {Student.numofStudent}')
+    print(f'Difference between s1 and s2 scores: {s1-s2}')
+
+    print(s1 > s2)
+    print(s1 < s2)
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -12,26 +12,27 @@ def test_main_1():
     # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
     # sys.stdin = io.StringIO(datastr)
 
-    r1 = main.Rectangle(10, 20)
-    r2 = main.Rectangle(100, 200)
-    print(r1)
-    print(r2)
-    assert r1.width == 10
-    assert r1.height == 20
-    r1._width = 99
-    r1._height = 99
-    print(r1)
-    print(r2)
-    assert r1.width == 99
-    assert r1.height == 99
+    sc1 = [100, 100, 100]
+    sc2 = [90, 90, 90]
 
-    assert (r1 < r2) == True
-    assert (r1 > r2) == False
+    s1 = main.Student('James', sc1)
+    s2 = main.Student('Mary', sc2)
+    print(s1)
+    print(s2)
+    print(f'Total number of students: {main.Student.numofStudent}')
+    print(f'Difference between s1 and s2 scores: {s1-s2}')
+
+    print(s1 > s2)
+    print(s1 < s2)
 
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
+
+    assert (s1 > s2) == True
+    assert (s1 < s2) == False
+    assert (s1-s2) == 10.0
 
     # regex_string = r'[\w,\W]*Elapsed'
     # regex_string += r'[\w,\W]*time'
